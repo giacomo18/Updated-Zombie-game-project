728a44f6b4ff681af2b2151a789e33ac8f8b74bf
giacomo18 <giacomo.okello22@gmail.com>
2022-05-13 17:04:38 +1000

    Updated zombie game

---
+++ project/Assets/Scripts/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    GameObject bulletSpawnPoint;
+    public bool canFire;
+    public float rateOfFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletSpawnPoint = transform.GetChild(0).gameObject;
+        canFire = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireShot();
+    }
+
+    public void fireShot()
+    {
+        if (Input.GetMouseButtonDown(0) && canFire)
+        {
+            StartCoroutine(FireRate());
+        }
+         
+        IEnumerator FireRate()
+        {
+            canFire = false;
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+            yield return new WaitForSeconds(rateOfFire);
+            canFire = true;
+        }
+            
+    }
+}
---
+++ project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+  //  private int highScore;
+  //  [HideInInspector] public int score;
+    [HideInInspector] public float health;
+    public float damageMultiplyer = 1.0f;
+ //   public Text highScoreText;
+ //   public Text scoreText;
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    highScore = PlayerPrefs.GetInt("HIGHSCORE");
+    //    highScoreText.text = "High Score: " + highScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDamage>().health; //This code finds the player, and the PlayerDamage script on them, then accesses the 'health' variable.
+     //   scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + health;
+
+     /*   if(highScore < score)
+        {
+            PlayerPrefs.SetInt("HIGHSCORE", score);
+            PlayerPrefs.Save();
+            highScore = score;
+            highScoreText.text = "High Score: " + highScore;
+        }
+     */
+    }
+
+}
